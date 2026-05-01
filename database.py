e246fe9c655708e876a398de2fec588608e65018
Ali Shahmeer <alishahmeerkhan05@gmail.com>
2026-05-01 19:44:23 +0500

    Added flask functionality, seperated database file, started login page.

---
+++ database.py
@@ -0,0 +1,22 @@
+import oracledb
+
+oracledb.init_oracle_client()
+
+pool = oracledb.create_pool(
+    user='smart_home_energy_manager',
+    password='fast',
+    dsn='192.168.18.97/xe',
+    min=2,
+    max=5,
+    increment=1
+)
+    
+def getUserData(userid):
+    with pool.acquire() as connection:
+        with connection.cursor() as cursor:
+            query = f'SELECT * FROM users WHERE userid = {userid}'
+            for r in cursor.execute(query):
+                return r
+            
+def createNewUser(userData):
+    pass
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import oracledb
+from flask import Flask, render_template, request
+from database import getUserData
 
-oracledb.init_oracle_client()
+app = Flask(__name__)
 
-with oracledb.connect(user='smart_home_energy_manager', 
-                      password='fast',
-                      dsn='192.168.18.97/XE') as connection:
-    with connection.cursor() as cursor:
-        query = 'SELECT * FROM users WHERE userid = 1'
-        for r in cursor.execute(query):
-            print(r)
+@app.route('/', methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        # The user clicked the "Log In" button!
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        # Here is where you will eventually call your database function
+        print(f"Attempting to log in user: {username}")
+        return "Form submitted successfully!"
+        
+    # If it's a GET request, just show the login page
+    return render_template('log_in.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
